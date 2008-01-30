dac12423764675cc8188438aca5e8675f92a0138
kawa0117 <kawa0117@535ef166-a844-0410-a506-13d30066e70f>
2008-01-30 18:44:05 +0000

    init.js added

--- jsonpzip/src/jsonpzip-main.js
+++ jsonpzip/src/jsonpzip-main.js
@@ -859,37 +859,3 @@ JsonpZip.Page = function () {
 JsonpZip.Page.prototype = new JsonpZip.Base();
 
 /* ********************************************************* */
-
-new function () {
-    var init = function () {
-        JsonpZip.page = new JsonpZip.Page();
-    };
-	if ( window.jQuery && jQuery.fn && jQuery.fn.ready ) {
-		jQuery.fn.ready( init );
-	} else {
-	    setTimeout( init, 0 );
-	}
-}
-
-/* ********************************************************* */
-
-
-
-/* ================================================================ *
-	var init = function () {
-		var chain = function ( ret ) { alert( ret ) };
-		var jzip = new JsonpZip.Core();
-		jzip.get_prefcd_by_addr( chain, '東京都' );
-		jzip.get_citycd_by_addr(  chain, "北海道空知郡上富良野町" );
-		jzip.get_city_by_citycd( chain, "01460" );
-		jzip.get_pref_by_prefcd( chain, 13 );
-		jzip.get_preflist( chain );
-		jzip.get_addrlist_by_zipcd( chain, "0110951" );
-		jzip.get_addr_by_zipcd( chain, "104-8001" );
-		jzip.get_ziplist_by_addr( chain, "東京都大田区仲池上" );
-		jzip.get_arealist_by_addr( chain, "東京都大田区" );
-		jzip.get_citylist_by_pref( chain, "東京都" );
-		jzip.get_citymeta_by_citycd( chain, "01101" );
-	};
-	setTimeout( init, 1 );
-* ================================================================ */
